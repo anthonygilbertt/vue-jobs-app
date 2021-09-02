b83131af2da39bc97f854f5dc75cda12a5f83057
Anthony <anthony.gilbert93@gmail.com>
2021-09-01 23:49:44 -0500

    Added Programmatic Navigation

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,30 @@
     <router-link :to="{ name: 'About'}">About</router-link> |
     <router-link :to="{ name: 'Jobs'}">Jobs</router-link>
   </div>
+
+  <button @click="redirect">Redurect</button>
+  <button @click="back">Go Back</button>
+  <button @click="forward">Go Forward</button>
+
+
   <router-view/>
 </template>
+
+<script>
+export default {
+  methods: {
+    redirect() {
+      this.$router.push({ name: 'Home'})
+    },
+    back() {
+      this.$router.go(-1)
+    },
+    forward() {
+      this.$router.go(+1)
+    }
+  }
+}
+</script>
 
 <style>
 #app {
@@ -32,5 +54,12 @@
 #nav a.router-link-exact-active {
   color: white;
   background: crimson;
+}
+button {
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
 }
 </style>
