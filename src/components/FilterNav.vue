278c90ba5c54e552b2d013b27cce31d449a2e534
Anthony <anthony.gilbert93@gmail.com>
2021-09-10 01:54:03 -0500

    Initial Project setup

---
+++ src/components/FilterNav.vue
@@ -0,0 +1,13 @@
+<template>
+  <H2>Filter Nav Component</H2>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/projects/EditProject.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>Edit Project</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
