9119ea96924102b82ab524955abb677d6e0bbee5
Anthony <anthony.gilbert93@gmail.com>
2021-09-11 02:15:17 -0500

    66. Edit Project Route: Added a form to the EditProject Page and made it populate the this.project data

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="project" :class="{ complete: project.complete}">
+  <div class="project" :class="{ complete: project.complete }">
     <div class="actions">
       <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
       <div class="icons">
-        <span class="material-icons">edit</span>
+        <router-link :to="{ name: 'EditProject', params:{ id: project.id} }">
+          <span class="material-icons">edit</span>
+        </router-link>
         <span @click="deleteProject" class="material-icons">delete</span>
         <span @click="toggleComplete" class="material-icons tick">done</span>
       </div>
     </div>
-
     <div v-if="showDetails" class="details">
       <p>{{ project.details }}</p>
     </div>
@@ -17,72 +18,64 @@
 
 <script>
 export default {
-  props: ["project"],
+  props: ['project'],
   data() {
     return {
       showDetails: false,
       uri: 'http://localhost:3000/projects/' + this.project.id
-    };
-  },
-  toggleDetails(showDetails) {
-    if ((showDetails = false)) this.showDetails = !showDetails;
+    }
   },
   methods: {
-    deleteProject(project) {
-      console.log('delete button clicked');
-      
-      fetch(this.uri, { method: 'DELETE'})
-        .then((e) = this.$emit('delete', this.project.id))
-        .catch(err => console.log(err.message))
+    deleteProject() {
+      fetch(this.uri, { method: 'DELETE' })
+        .then(() => this.$emit('delete', this.project.id))
+        .catch(err => console.log(err))
     },
-    toggleComplete(project) {
-      console.log('Complete button clicked');
-
+    toggleComplete() {
       fetch(this.uri, {
         method: 'PATCH',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({ complete: this.project.complete = !this.project.complete })
-        })
-        .then((e) => {
-          this.$emit('toggleComplete', this.project.id)
-        })
-        .catch(err => console.log(err.mesage))
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ complete: !this.project.complete })
+      }).then(() => {
+        this.$emit('complete', this.project.id)
+      }).catch(err => console.log(err))
     }
-
   }
+  
 };
 </script>
 
-<style>
-.project {
-  margin: 20px auto;
-  background: white;
-  padding: 10px 20px;
-  border-radius: 4px;
-  box-shadow: 1px 2px 3pc rgba(0, 0, 0, 0.5);
-  border-left: 4px solid #e90074;
-}
-h3 {
-  cursor: pointer;
-}
-.actions {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-.material-icons {
-  font-size: 24px;
-  margin-left: 10px;
-  color: #bbb;
-  cursor: pointer;
-}
-.material-icons:hover {
-  color: #777;
-}
-.project.complete {
-  border-left: 4px solid #2d9236;
-}
-.project.complete .tick{
-  color:  #2d9236;
-}
+<style scoped>
+  .project {
+    margin: 20px auto;
+    background: white;
+    padding: 10px 20px;
+    border-radius: 4px;
+    box-shadow: 1px 2px 3px rgba(0,0,0,0.05);
+    border-left: 4px solid #e90074;
+  }
+  h3 {
+    cursor: pointer;
+  }
+  .actions {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .material-icons {
+    font-size: 24px;
+    margin-left: 10px;
+    color: #bbb;
+    cursor: pointer;
+  }
+  .material-icons:hover {
+    color: #777;
+  }
+  /* completed projects */
+  .project.complete {
+    border-left: 4px solid #00ce89;
+  }
+  .project.complete .tick {
+    color: #00ce89;
+  }
 </style>
